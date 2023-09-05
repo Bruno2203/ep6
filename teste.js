2e41f5f40f3718618447401c47c56e94e3bb9f5d
Bruno2203 <125492134+Bruno2203@users.noreply.github.com>
2023-09-05 00:58:11 -0300

    Add files via upload

---
+++ teste.js
@@ -0,0 +1,36 @@
+const previousOperationsText = document.querySelector("#prvious-operation")
+const currentOperationsText = document.querySelector("#current-operation")
+const button = document.querySelectorAll("#button_conteiner button")
+
+class calculator{
+    constructor(previousOperationsText, currentOperationsText) {
+        this.previousOperationsText = previousOperationsText
+        this.currentOperationsText = currentOperationsText
+        this.currentOperation = ""    
+    }
+    //add umdigito para fazer alguma coisa
+    addDigit(digit) {
+
+        this.currentOperation = digit
+        this.updateScreen()
+    }
+
+    //altera valores da tela da calculadora
+    updateScreen() {
+        this.currentOperationsText.innertext += this.currentOperation;
+    }
+}
+
+const calc = new calculator(previousOperationsText, currentOperationsText);
+
+buttons.forEach((btn) =>{
+    btn.addEventListener("click", (e) => {
+        const value = e.target.innertext
+
+        if(+value >= 0 || value ==="."){
+            calc.addDigit(value)
+        } else{
+            console.log("Op: "+ value)
+        }
+    })
+})
