6903c5bea4718f7a2fe413908a83d5efa877ccff
Bruno2203 <125492134+Bruno2203@users.noreply.github.com>
2023-09-28 16:16:49 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,7 @@
+    function coferir(){
+        var respostaCorreta = "5"
+        var respostaUsuario = document.querySelector('input[name"resposta"]')
+    }
+    if(respostaUsuario === respostaCorreta){
+        document
+    }
