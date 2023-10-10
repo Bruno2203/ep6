7d1855669e0f8a3a60e81370ea398664ee31c9f8
Bruno2203 <125492134+Bruno2203@users.noreply.github.com>
2023-10-10 16:06:06 -0300

    Delete ep6-main directory

--- ep6-main/java.js
+++
@@ -1,6 +0,0 @@
-function funcaoresposta(){
-    var respostaUsario = document.querySelector('input[name="respota"]:checked').value;
-    if(respostaUsario.value == "correta"){
-        document.getElementById("retorno").textContent = "acertou";
-    }
-}
