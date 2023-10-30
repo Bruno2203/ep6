56811a14a3a1bed379f0dad8c7da4525ad913e62
Bruno2203 <125492134+Bruno2203@users.noreply.github.com>
2023-10-30 18:44:29 -0300

    Add files via upload

--- java.js
+++ java.js
@@ -72,14 +72,14 @@ function funcao_resposta(){
             contador = contador + 1;
         }
         else{
-            document.getElementById("resp_errada_1").innerHTML = "Errada, A velocidade média é calculada dividindo o deslocamento pela variação de tempo.";
+            document.getElementById("resp_errada_1").innerHTML = "Errada, pois a divisão de 240 por 3 é igual a 80.";
         }
         if(resp2.value == "correta"){
             document.getElementById("resp_correta_2").innerHTML = "Correta!";
             contador = contador + 1;
         }
         else{
-            document.getElementById("resp_errada_2").innerHTML = "Errada,  A velocidade média é calculada dividindo o deslocamento pela variação de tempo.";
+            document.getElementById("resp_errada_2").innerHTML = "Errada, pois a divisão de 400 por 80 é igual a 5.";
 
         }
         if(resp3.value == "correta"){
@@ -87,7 +87,7 @@ function funcao_resposta(){
             contador = contador + 1;
         }
         else{
-            document.getElementById("resp_errada_3").innerHTML = "Errada,  A velocidade média é calculada dividindo o deslocamento pela variação de tempo.";
+            document.getElementById("resp_errada_3").innerHTML = "Errada, pois a divisão de 1200 por 2 é igual a 600.";
 
         }
         if(resp4.value == "correta"){
@@ -95,7 +95,7 @@ function funcao_resposta(){
             contador = contador + 1;
         }
         else{
-            document.getElementById("resp_errada_4").innerHTML = "Errada,  A velocidade média é calculada dividindo o deslocamento pela variação de tempo.";
+            document.getElementById("resp_errada_4").innerHTML = "Errada,  pois a divisão de 15 por 45 é igual a 0,333333...";
 
         }
         if(resp5.value == "correta"){
@@ -103,7 +103,8 @@ function funcao_resposta(){
             contador = contador + 1;
         }
         else{
-            document.getElementById("resp_errada_5").innerHTML = "Errada,  A velocidade média é calculada dividindo o deslocamento pela variação de tempo.";
+            document.getElementById("resp_errada_5").innerHTML = "Errada,  pois a divisão de 150 por 5 é igual a 30";
+
 
         }
         if(resp6.value == "correta"){
@@ -111,7 +112,8 @@ function funcao_resposta(){
             contador = contador + 1;
         }
         else{
-            document.getElementById("resp_errada_6").innerHTML = "Errada,  A velocidade média é calculada dividindo o deslocamento pela variação de tempo.";
+            document.getElementById("resp_errada_6").innerHTML = "Errada,  pois a divisão de 240 por 4 é igual a 60";
+
 
         }
         if(resp7.value == "correta"){
@@ -119,7 +121,7 @@ function funcao_resposta(){
             contador = contador + 1;
         }
         else{
-            document.getElementById("resp_errada_7").innerHTML = "Errada,  A velocidade média é calculada dividindo o deslocamento pela variação de tempo.";
+            document.getElementById("resp_errada_7").innerHTML = "Errada,  pois a divisão de 15km por 45min é igual a 10km/h";
 
         }
         if(resp8.value == "correta"){
@@ -127,7 +129,7 @@ function funcao_resposta(){
             contador = contador + 1;
         }
         else{
-            document.getElementById("resp_errada_8").innerHTML = "Errada,  A velocidade média é calculada dividindo o deslocamento pela variação de tempo.";
+            document.getElementById("resp_errada_8").innerHTML = "Errada,  pois a divisão de 5000m por 20min é igual a 4,17m/s";
 
         }
         if(resp9.value == "correta"){
@@ -135,14 +137,15 @@ function funcao_resposta(){
             contador = contador + 1;
         }
         else{
-            document.getElementById("resp_errada_9").innerHTML = "Errada,  A velocidade média é calculada dividindo o deslocamento pela variação de tempo.";
+            document.getElementById("resp_errada_9").innerHTML = "Errada,  pois a divisão de 480 por 8 é igual a 60.";
+
         }
         if(resp10.value == "correta"){
             document.getElementById("resp_correta_10").innerHTML = "Correta!";
             contador = contador + 1;
         }
         else{
-            document.getElementById("resp_errada_10").innerHTML = "Errada,  A velocidade média é calculada dividindo o deslocamento pela variação de tempo.";
+            document.getElementById("resp_errada_10").innerHTML = "Errada,  pois a divisão de 300 por 2,5 é igual a 200";
 
         }
         document.getElementById("quantos_acertos").innerHTML = "Você acertou " + contador + " questões!";
